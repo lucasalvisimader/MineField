0d7b69fae6f7809acde19586d0a90e42047f6db3
Lucas <105462862+Lucas-Mader@users.noreply.github.com>
2023-09-25 20:42:13 -0300

    mui level header update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,41 @@
 import './Header.css';
 import '../../custom/Color.css'
 
+// react
+import { useState } from 'react'
+
+// mui
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 const Header = () => {
+    const [age, setAge] = useState('');
 
-    return(<>
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value);
+    };
+
+    return (<>
         <div className="header__container">
-
+            <FormControl sx={{ m: 1, minWidth: 80 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value={age}
+                    onChange={handleChange}
+                    autoWidth
+                    label="Age">
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Twenty</MenuItem>
+                    <MenuItem value={21}>Twenty one</MenuItem>
+                    <MenuItem value={22}>Twenty one and a half</MenuItem>
+                </Select>
+            </FormControl>
         </div>
     </>);
 }
